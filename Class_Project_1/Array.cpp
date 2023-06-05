31dc1ab2dcb89e26b85ed185d3c0d401153368d3
Leoardo Dorantes <133131159+LDorantes@users.noreply.github.com>
2023-06-05 10:47:19 -0600

    Update Array.cpp

--- Class_Project_1/Array.cpp
+++ Class_Project_1/Array.cpp
@@ -8,25 +8,7 @@ RawArray::RawArray(int in_Size) : Size(in_Size)
 {
 	InitialElement = new int[Size];
 
-	// Restriccion del tamano del arreglo. 
-
-	if (Size > 6)
-	{
-		std::cout << "Advertencia: El Size especificado es mayor que el numero de valores asignados. "\
-			"Los valores restantes se inicializaran a 0." << std::endl;
-
-
-
-		// Inicializar los valores restantes en 0
-		for (int i = 6; i < Size; i++)
-		{
-			InitialElement[i] = 0;
-		}
-	}
-	else if (Size < 6) {
-		std::cout << "Error: El size especificado es menor que el size minimo requerido (6)." << std::endl;
-		return;
-	}
+	
 }
 
 
@@ -114,4 +96,102 @@ void RawArray::TestCaseReplace(int Valores)
 	InitialElement[3] = 0;
 	InitialElement[4] = 1;
 	InitialElement[5] = 5;
+	// Restriccion del tamano del arreglo. 
+
+	if (Size > 6)
+	{
+		std::cout << "Advertencia: El Size especificado es mayor que el numero de valores asignados. "\
+			"Los valores restantes se inicializaran a 0." << std::endl;
+
+
+
+		// Inicializar los valores restantes en 0
+		for (int i = 6; i < Size; i++)
+		{
+			InitialElement[i] = 0;
+		}
+	}
+	else if (Size < 6) {
+		std::cout << "Error: El size especificado es menor que el size minimo requerido (6)." << std::endl;
+		return;
+	}
+}
+
+// A PARTIR DE AQUI SE CONSTRUYERON LAS FUNCIONES PARA REALIZAR EL EXAMEN.
+
+//Asignar valor V a uno de cada X elementos del RawArray, donde X y V son un entero.
+void RawArray::AssignEveryXElements(int v, int x)
+{
+	int* auxPosition = InitialElement;
+	int counter = 1;
+
+	for (int i = 0; i < Size; i++)
+	{
+		if (counter == x)
+		{
+			*auxPosition = v;
+			counter = 1;
+		}
+		else
+		{
+			counter++;
+		}
+		auxPosition++;
+	}
+
+}
+
+//Ordena los elementos del RawArray de menor a mayor
+void RawArray::SortFunction()
+{
+
+}
+
+//Añade al RawArray "A" todos los elementos del RawArray "B", Manteniendo los elementos de "A" al principio y despues agregando los elementos de "B"
+void AppendArray(RawArray arrayToAppend)
+{
+
+}
+
+//Modifica el tamano del RawArray para igualar el numero de elementos de newSize. 
+//Si newSize es mayor al Size actual del RawArray, Conserva el total de sus Size_elementos 
+//Si newSize es menor al Size actual del RawArray; conserva los primeros elementos hasta newSize 
+void RawArray::SetSize(int newSize)
+{
+
+}
+
+//Añade los arrayToInsert.Size-elementos al RawArray que llamó la función Insert, a partir del elemento en startIndex.
+void RawArray::Insert(RawArray arrayToInsert, int startIndex)
+{
+
+}
+
+//Suma elemento por elemento los valores del Array "A" con los valores del array "B". 
+//A y B tienen el mismo Size. 
+//Retorna un RawArray distinto de A y B con los resultados de la suma.
+void RawArray::SumArrays(RawArray A, RawArray B)
+{
+
+}
+
+//Recibe un valor X, si encuentra un elemento con dicho valor dentro del RawArray, regresa la posición de dicho elemento.
+//Si no encuentra el valor X dentro del RawArray, regresa -1.
+int RawArray::GetIndexOf(int x)
+{
+	return -1;
+}
+
+//Regresar la posición del último elemento del RawArray con el valor X dado. 
+//Si no encuentra ningun elemento con valor x, regresar -1.
+int RawArray::GetLastOf(int x)
+{
+	return -1;
+}
+
+//Recibe un valor X; regresa el indice de todos los elementos con valor X
+//Si no encuentra ningun elemento condicho valor, regresa un array cuyo primer y único elemento es -1.
+RawArray RawArray::GetIndicesOf(int x)
+{
+	return NULL;
 }
